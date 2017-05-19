96ea43f7ed4012061191f23ca8a6d072b4316c54
Weng <wenganxiang@163.com>
2017-05-19 11:27:45 +0800

    完成遍历省市县三级列表功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by Administrator on 2017/5/15.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
